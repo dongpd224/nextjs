0826deeebf45e291987275251920df902a36048b
Dong <dongpd224@gmail.com>
2021-11-30 17:52:00 +0700

    fetching data

--- content-manager-app/pages/api/resources.js
+++ content-manager-app/pages/api/resources.js
@@ -1,6 +1,17 @@
 
 export default async function(req, res) {
-  const API = await fetch('http://localhost:3001/api/resources')
-  const data = await API.json()
-  res.send(data);
+  if(req.method === "GET"){
+
+    const API = await fetch('http://localhost:3001/api/resources')
+    const data = await API.json()
+    res.send(data);
+  }
+  if(req.method === "POST"){
+    const {title,description,link,timeToFinish,priority} = req.body
+    if(!title || !description || !link || !timeToFinish || !priority){
+      return res.status(422).send("Data are missing")
+    }
+    res.send("Data has been received")
+  }
 }
+
