ae0f1e0fedfec8d67f9d2a9b11cfb0f8a84126ca
dongpd224 <dongpd224@gmail.com>
2021-12-01 23:24:55 +0700

    update resourceList

--- content-manager-app/components/ResourceList.js
+++ content-manager-app/components/ResourceList.js
@@ -1,26 +1,30 @@
+import Link from "next/dist/client/link"
 const ResourceList = ({ resources }) => {
   return (
     <>
       <section className="hero ">
-        <div className="hero-body">
-          <div className="container">
+      <div className="hero-body">
+        <div className="container">
+          <section className="section">
+            <div className="columns is-multiline is-variable is-8">
+                {resources.map(resource => (
+                  <div key={resource.id} className="column is-5 is-offset-1 ">
+                  <div className="content is-medium">
+                    <h2 className="subtitle is-5 has-text-grey">
+                      {resource.createdAt}
+            
+                    </h2>
+                    <h1 className="title has-text-black is-3">{resource.title}</h1>
+                    <p className="has-text-dark mb-2">{resource.description}</p>
+                    <Link href={`/resources/${resource.id}`}>
+                      <a className="button is-light">
+                        Details
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+                ))}
 
-            <section className="section">
-              <div className="columns is-variable is-8">
-                {resources.map((resource, index) => {
-                    <div className="column is-5 is-offset-1" key={index}>
-                      <div className="content is-medium"></div>
-                      <h2 className="subtitle is-5 has-text-grey">{resource.timeToFinish}</h2>
-                      <h1 className="title has-text-black is-3">{resource.title}</h1>
-                      <p className="has-text-dark">{resource.description}</p>
-                    </div>
-                })}
-               {resources.map(item =>(
-                 <div>
-                   </div>
-               ))
-
-               }
               </div>
             </section>
 
