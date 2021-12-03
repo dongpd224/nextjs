870a65050b5c6f21ff8d7b4fddc6147d42fc2a28
Dong <dongpd224@gmail.com>
2021-12-03 11:47:34 +0700

    route path

--- content-manager-app/pages/resources/[id].js
+++ content-manager-app/pages/resources/[id].js
@@ -24,7 +24,22 @@ function ResourceDetail({resource}) {
         </Layout>
     )
 }
-export async function getServerSideProps({params}){
+export async function getStaticPaths(){
+    const resData = await fetch("http://localhost:3001/api/resources")
+    const data = await resData.json()
+    const paths = data.map(resource =>{
+        return{
+            params : {id: resource.id}
+        }
+    })
+    return {
+        paths,
+        // Fallback: False. Neu duong dan bi sai thi tra lai trang 404
+        // Fallback: True. Neu duong dan bi sai, thi sever get Data lai roi kiem tra, neu ko co thi 404
+        fallback: false
+    }
+}
+export async function getStaticProps({params}){
     const dataRes = await fetch(`http://localhost:3001/api/resources/${params.id}`)
     const data = await dataRes.json()
     return{
@@ -33,4 +48,12 @@ export async function getServerSideProps({params}){
         }
     }
 }
+
+// ResourceDetail.getInitialProps = async ({query}) =>{
+//     const dataRes = await fetch(`http://localhost:3001/api/resources/${query.id}`)
+//         const data = await dataRes.json()
+//         return{
+//                 resource : data
+//         }
+// }
 export default ResourceDetail
